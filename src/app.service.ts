67ebc9441fff87e436ed3a1840829656a73284e3
Roman Sheviakov <roman_sheviakov@epam.com>
2024-10-14 15:01:09 +0500

    feat: Add @nestjs/config dependency and configure global usage

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const envValue = process.env.YAAPI_BEARER_TOKEN;
+    return `Hello World! ${envValue}`;
   }
 }
