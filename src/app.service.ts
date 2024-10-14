5cfc6d7af34055cb0af8a9be480639f732928cf9
Roman Sheviakov <roman_sheviakov@epam.com>
2024-10-14 15:14:16 +0500

    fix: remove test code

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    const envValue = process.env.YAAPI_BEARER_TOKEN;
-    return `Hello World! ${envValue}`;
+    return `Hello World!`;
   }
 }
