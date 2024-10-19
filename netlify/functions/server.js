413454dcb70d0cba0df914f585761cad3452ff9e
Roman Sheviakov <roman_sheviakov@epam.com>
2024-10-19 20:31:15 +0500

    refactor: update import path for main module in server.js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,10 +1,21 @@
-const { createServer, proxy } = require('aws-serverless-express');
+const awsServerlessExpress = require('aws-serverless-express');
 const express = require('express');
-const { AppModule } = require('dist/main.js'); // Adjust the path to your main module
+const { NestFactory } = require('@nestjs/core');
+const { AppModule } = require('../../dist/app.module'); // Adjust the path to your main module
+
+async function createNestServer(expressInstance) {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance),
+  );
+  await app.init();
+}
 
 const app = express();
-const server = createServer(app);
+createNestServer(app);
+
+const server = awsServerlessExpress.createServer(app);
 
 module.exports.handler = (event, context) => {
-  return proxy(server, event, context);
+  return awsServerlessExpress.proxy(server, event, context);
 };
