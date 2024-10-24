6bcab5bd3c9df304d12d218f24b8de094f6992bf
Roman Sheviakov <roman_sheviakov@epam.com>
2024-10-24 17:12:29 +0500

    refactor: enable CORS with specific options in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+const corsOptions: CorsOptions = {
+  origin: ['https://mineralmagic.ru/'],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: 'Content-Type, Accept, Authorization',
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { abortOnError: false });
+  app.enableCors(corsOptions);
+
   await app.listen(3000);
 }
 bootstrap();
